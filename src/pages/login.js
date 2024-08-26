2433cd2a976810eef6b18fc6c0ad8556bffbaab2
Kesava <kesava.infos@gmail.com>
2024-08-27 00:47:36 +0530

    Login Page code updated

--- src/pages/login.js
+++ src/pages/login.js
@@ -7,15 +7,15 @@ import ButtonField from "../components/ButtonField";
 export default function Login() {
     return <div className="bg-[#f2f2f2] h-screen flex gap-1 p-4 sm:p-8 md:p-4">
         <div className="w-1/2 flex hidden sm:block relative">
-            <img src={bg} alt="Background Image" className="self-end w-100 absolute bottom-[-10px] right-[-5px]" />
+            <img src={bg} alt="Background Image" className="self-end w-100 absolute top-[100px] right-[-5px]" />
         </div>
-        <div className="flex flex-col items-center gap-2 justify-start min-w-[380px] bg-[#03234D]  h-[96%] rounded-3xl grow p-6">
+        <div className="flex flex-col items-center gap-2 justify-start min-w-[320px] bg-white  h-[96%] rounded-3xl grow p-6">
             <img src={logo} alt="logo" className="w-[150px]" />
-            <div className="flex flex-col gap-4 bg-white rounded-3xl w-[100%] text-white">
+            <div className=" bg-[#03234D] flex flex-col gap-4 rounded-3xl w-[100%] text-white">
                 <p className="  text-right pt-5 px-5 text-[#F86167] ">Login / Sign up</p>
                 <div className="p-5 w-[100%] items-center flex flex-col gap-5 ">
-                <InputTextField color="#F86167" border="#03234D" label="User Name" />
-                <InputTextField color="#F86167" border="#03234D" label="Password" />
+                <InputTextField color="#F86167" border="#fff" label="User Name" />
+                <InputTextField color="#F86167" border="#fff" label="Password" />
                 <p className="text-left text-[#F86167]">Forgot Password ?</p>
                 <ButtonField bg="#F86167" label="Login"  />
                 </div>
