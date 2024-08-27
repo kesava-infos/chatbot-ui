fafed6830717114f6a0eb9ec0b7f5e2c5431c8d3
Kesava <kesava.infos@gmail.com>
2024-08-27 18:04:29 +0530

    override  basic input field styles

--- src/components/common/InputTextField.js
+++ src/components/common/InputTextField.js
@@ -7,56 +7,60 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff';
 export default function InputTextField(props) {
     const [showPassword, setShowPassword] = React.useState(false);
 
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
-    return  <div className="flex relative">
-    <TextField
-    type={showPassword ? "text" : props.type}
-    style={{width:"100%"}}
-    sx={{
-        "& .MuiFormControl-root.MuiTextField-root":{
-            width: "100%"
-        },
-        "& .MuiOutlinedInput-root": {
-            color: "#fff",
-            width: "100%",
-            fontWeight: "bold",
-            background: props.bg,
-            "& .MuiOutlinedInput-notchedOutline": {
-                color: props.color,
-                borderColor: props.border,
-                borderWidth: props.borderWidth,
-                borderRadius: '30px'
-            },
-            "&.Mui-focused": {
-                borderColor: props.border,
-                "& .MuiOutlinedInput-notchedOutline": {
-                    borderColor: props.border,
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+    return <div className="flex relative">
+        <TextField
+            size="small"
+            type={showPassword ? "text" : props.type}
+            style={{ width: "100%" }}
+            sx={{
+                "& .MuiFormControl-root.MuiTextField-root": {
+                    width: "100%"
                 },
-            },
-        },
-        "& .MuiInputLabel-outlined": {
-            color: props.color,
-            border: "1px",
-            "&.Mui-focused": {
-                fontWeight: "bold",
-                border: "1px"
-            },
+                "& .MuiOutlinedInput-root": {
+                    color: "#fff",
+                    width: "100%",
+                    fontWeight: "bold",
+                    background: props.bg,
+                    "&:hover fieldset": {
+                        borderColor: "#ccc"
+                    },
+                    "& .MuiOutlinedInput-notchedOutline": {
+                        color: props.color,
+                        borderColor: props.border,
+                        borderWidth: props.borderWidth,
+                        borderRadius: '30px',
+                    },
+                    "&.Mui-focused": {
+                        borderColor: props.border,
+                        "& .MuiOutlinedInput-notchedOutline": {
+                            borderColor: props.border,
+                        },
+                    },
+                },
+                "& .MuiInputLabel-outlined": {
+                    color: props.color,
+                    border: "1px",
+                    "&.Mui-focused": {
+                        fontWeight: "bold",
+                        border: "1px"
+                    },
+                }
+            }}
+            label={props.label} variant="outlined" />
+        {
+            props.type == "password" && <IconButton
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+                style={{ color: "#fff", position: "absolute", right: "20px", top: "2px" }}
+            >
+                {showPassword ? <VisibilityOff fontSize="small" /> : <Visibility fontSize="small" />}
+            </IconButton>
         }
-    }}
-    label={props.label} variant="outlined" />
-    {
-        props.type == "password" && <IconButton
-        onClick={handleClickShowPassword}
-        onMouseDown={handleMouseDownPassword}
-        edge="end"
-        style={{color: "#fff", position: "absolute", right:"20px", top: "10px"}}
-      >
-        {showPassword ? <VisibilityOff fontSize="small"/> : <Visibility fontSize="small" />}
-      </IconButton>
-    }
     </div>
 } 
